41a658d8dbcc259a76873956779edc8179256edd
Лысанов Дмитрий Александрович <lysanovda@admkuz.local>
2020-02-12 14:08:39 +0300

    Add date and time

--- backup_postgresql.ps1
+++ backup_postgresql.ps1
@@ -1,5 +1,7 @@
 # Скрипт создания бэкапов PostgreSQl
 # минимальная версия PowerShell 5.1. С версией ниже встроенная архивация не работает
+Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Start script"
+Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Initilizacion variable"
 # Текущая дата
 $date = Get-Date -format "yyyy-MM-dd"
 $config = Get-Content config.json | ConvertFrom-Json
@@ -12,10 +14,11 @@ $env:PGPASSWORD = $config.psql_srv.password
 # Устанавливаем рабочий каталог с сиполняемыми файлами PostgreSQL
 Set-Location $config.psql_srv.path_bin
 # Получаем список БД сервера PostgreSQL
-Write-Host "Get list DB in $temp_bd_list"
+Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Get list DB in $temp_bd_list"
 .\psql.exe -A -q -t -c "select datname from pg_database" > $temp_bd_list
 $name_bd_list = get-content $temp_bd_list
 # Удаляем временный файл
+Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Remove temp file list bd"
 Remove-Item $temp_bd_list -Recurse
 foreach ($name_bd in $name_bd_list) {
     # Проверяем имя БД с системными БД
@@ -23,12 +26,13 @@ foreach ($name_bd in $name_bd_list) {
         # Получаем имя файла архивной копии
         $name_bd_file=$config.path_backup+$date+"_"+$name_bd+".sql"
         # Дамп БД
-        Write-Host "Dump $name_bd in $name_bd_file"
+        Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Dump $name_bd in $name_bd_file"
         .\pg_dump.exe -Fc -b -f $name_bd_file $name_bd       
     }
 }
 # Проверяем необходимость сжатия дампа 
 if ($config.compress_zip) {
+    Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Start create zip arhive"
     # Получаем списко файлов для архивации
     $list_file_sql = [IO.Directory]::EnumerateFiles($config.path_backup,'*.sql')
     foreach ($file_sql in $list_file_sql) {
@@ -36,15 +40,17 @@ if ($config.compress_zip) {
         $full_file_sql = $config.$path_backup+$file_sql
         # Получаем имя сжатого дампа БД
         $zip_name_bd_file=$full_file_sql+".zip"
-        Write-Host "Greate arhive $full_file_sql in $zip_name_bd_file"
+        Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Greate arhive $full_file_sql in $zip_name_bd_file"
         # Сжимаем дамп
         Compress-Archive -Path $full_file_sql -DestinationPath $zip_name_bd_file -CompressionLevel Optimal
         # Удаляем не сжатый дамп
+        Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Delete file $full_file_sql"
         Remove-Item $full_file_sql -Recurse
     }
 }
 # Удаляем дампы страрше $lifetime_backup
 # Вычисляем дату после которой будем удалять файлы.
+Write-Host $(Get-Date -format "yyyy-MM-dd HH:mm") "Delete old file"
 $CurrentDay = Get-Date
 $ChDaysDel = $CurrentDay.AddDays($config.lifetime)
 Get-ChildItem -Path $config.path_backup -Recurse | Where-Object { $_.CreationTime -LT $ChDaysDel } | Remove-Item -Recurse -Force
